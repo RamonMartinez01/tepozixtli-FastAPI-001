604aaa288e904774449963540b8b06f033d4942b
Ramon Martinez <martinezcjr1@gmail.com>
2026-06-04 19:31:32 -0700

    feat(redis): agrega otro archivo para probar la conexión con redis, pero este archivo es un router. Relates #31

--- fastapi/app/api/router.py
+++ fastapi/app/api/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.routes import region
 from app.api.routes import ndvi_region_stats
 from app.api.routes import municipio
+from app.test_redis_route import router as test_router
 
 # Este es el router maestro que agrupará a todos los demás
 api_router = APIRouter()
@@ -16,3 +17,6 @@ api_router.include_router(region.router, prefix="/regiones", tags=["Regiones Esp
 api_router.include_router(ndvi_region_stats.router, tags=["Internal Microservices"])
 
 api_router.include_router(municipio.router, prefix="/municipios", tags=["Municipios"])
+
+# test de redis
+api_router.include_router(test_router)
